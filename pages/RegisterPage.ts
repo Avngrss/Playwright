965dbb5f77dc4821110b25956276eac47c79276a
avngrss <sedow94@mail.ru>
2025-04-04 09:49:42 +0300

    create a register fnc

--- pages/RegisterPage.ts
+++ pages/RegisterPage.ts
@@ -1,23 +1,40 @@
 import { BasePage } from "./BasePage";
 
-export class RegisterPage extends BasePage{
-   
-    firstNameField = () => this.page.locator(`[data-test="first-name"]`)
-    lastNameField = () => this.page.locator(`[data-test="last-name"]`)
-    dateOfBirthdayFiled = () => this.page.locator(`[data-test="dob"]`)
-    streetField = () => this.page.locator(`[data-test="street"]`)
-    postCodeField = () => this.page.locator(`[data-test="postal_code"]`)
-    cityField = () => this.page.locator(`[data-test="city"]`)
-    stateField = () => this.page.locator(`[data-test="state"]`)
-    countryField = () => this.page.locator(`[data-test="country"]`)
-    phoneField = () => this.page.locator(`[data-test="phone"]`)
-    emailField = () => this.page.locator(`[data-test="email"]`)
-    passwordField = () => this.page.locator(`[data-test="password"]`)
-    registerButton = () => this.page.locator(`[data-test="register-submit"]`)
-    errorPassword = () => this.page.locator(`[data-test="password-error"]`)
+export class RegisterPage extends BasePage {
+  //Locators
+  firstNameField = () => this.page.locator(`[data-test="first-name"]`);
+  lastNameField = () => this.page.locator(`[data-test="last-name"]`);
+  dateOfBirthdayFiled = () => this.page.locator(`[data-test="dob"]`);
+  streetField = () => this.page.locator(`[data-test="street"]`);
+  postCodeField = () => this.page.locator(`[data-test="postal_code"]`);
+  cityField = () => this.page.locator(`[data-test="city"]`);
+  stateField = () => this.page.locator(`[data-test="state"]`);
+  countryField = () => this.page.locator(`[data-test="country"]`);
+  phoneField = () => this.page.locator(`[data-test="phone"]`);
+  emailField = () => this.page.locator(`[data-test="email"]`);
+  passwordField = () => this.page.locator(`[data-test="password"]`);
+  registerButton = () => this.page.locator(`[data-test="register-submit"]`);
+  //Error locators
+  errorPassword = () => this.page.locator(`[data-test="password-error"]`);
+  existedUser = () => this.page.locator(`.help-block`);
 
-    async goto() {
-        await this.gotoRegister()
-    }
+  //Actions
+  async goto() {
+    await this.gotoRegister();
+  }
+
+  async registerNewUser() {
+    await this.firstNameField().fill("TestUser");
+    await this.lastNameField().fill("TestLastName");
+    await this.dateOfBirthdayFiled().fill("2000-04-03");
+    await this.streetField().fill("My street 22");
+    await this.postCodeField().fill("223456");
+    await this.cityField().fill("Moscow");
+    await this.stateField().fill("Alaska")
+    await this.countryField().selectOption("Aruba");
+    await this.phoneField().fill("324534");
+    await this.emailField().fill(`jane${Date.now()}@example.com`);
+    await this.passwordField().fill("NyGosuPass1@");
+    await this.registerButton().click();
+  }
 }
-
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,19 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { RegisterPage } from "../pages/RegisterPage";
-import { LoginPage } from '../pages/LoginPage';
 
 
 test.describe("Register new user", () => {
-
     test.beforeEach(async({page}) => {
         const registerPage = new RegisterPage(page)
-        const loginPage = new LoginPage(page)
-
-        loginPage.goto()
+        registerPage.gotoRegister()
     })
 
     test("Successful account creation usring all form fields", async ({page}) => {
-
+        const registerPage = new RegisterPage(page)
+        await registerPage.registerNewUser()
+        await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login')
     })
 }) 
 
